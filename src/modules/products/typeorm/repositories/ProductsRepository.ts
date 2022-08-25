2f47bd4ee4efcd220430510c4f68ce3e28856f9f
jacsonrsasse <jacson.sasse@hotmail.com>
2022-08-25 11:06:31 -0300

    First repository created

---
+++ src/modules/products/typeorm/repositories/ProductsRepository.ts
@@ -0,0 +1,26 @@
+import { EntityRepository, Repository } from "typeorm";
+import Product from '../entities/Product';
+
+/**
+ * Repositório customizado, explicitando que ele gerencia
+ * a entidade de Produto
+ */
+@EntityRepository(Product)
+export class ProductRepository extends Repository<Product> {
+
+    /**
+     * Encontra um produto a partir do nome dele
+     * @param name Nome do produto desejado
+     * @returns Objeto contendo os dados do produto, ou undefined
+     */
+    public async findByName(name: string): Promise<Product | undefined> {
+        const product = this.findOne({
+            where: {
+                name
+            }
+        });
+
+        return product;
+    }
+
+}
