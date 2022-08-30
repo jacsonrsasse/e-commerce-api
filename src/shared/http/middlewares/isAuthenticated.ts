5adc32b0bdc6a502b2596d4f9ca956a79da401ca
jacsonrsasse <jacson.sasse@hotmail.com>
2022-08-29 21:46:25 -0300

    tsconfig adjusted, created folder @types and implemented in function isAuthenticated

--- src/shared/http/middlewares/isAuthenticated.ts
+++ src/shared/http/middlewares/isAuthenticated.ts
@@ -3,6 +3,12 @@ import { NextFunction, Request, Response } from 'express';
 import { verify } from 'jsonwebtoken';
 import authConfig from '@config/auth';
 
+interface ITokenPayload {
+    iat: number;
+    exp: number;
+    sub: string;
+}
+
 /**
  * Middleware function to verify if the token is valid
  */
@@ -16,7 +22,21 @@ export default function isAuthenticated(request: Request, response: Response, ne
     const [bearer, token] = authHeader.split(' ');
 
     try {
-        const decodedToken = verify(token, authConfig.jwt.secret);
+        const decodedToken = verify(token, authConfig.jwt.secret) as ITokenPayload;
+        const { sub } = decodedToken;
+
+        /**
+         * Toda requisição para rotas que utilizarem este middleware, terão em seu
+         * objeto Request, o ID do usuário que veio no payload do token, desta forma
+         * essa informação acaba sendo transferida por toda a aplicação.
+         *
+         * Para que functione corretamente, o objeto foi "sobrescrito". Vide a pasta
+         * '@types/express'
+         */
+        request.user = {
+            id: sub,
+        };
+
         return next();
     } catch {
         throw new Exception('Invalid JWT Token!');
